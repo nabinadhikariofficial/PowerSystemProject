f81322de081791c92118480d999cff5569a2b704
Nabin Adhikari <nabinadhikari70@gmail.com>
2023-03-16 23:00:52 +0545

    large file

---
+++ generator.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+
+
+class DataProcessor():
+    def __init__(self, data_name):
+        self.data = pd.read_csv('data/'+data_name+".csv", index_col=0)
+        self.cols = self.data.columns.tolist()
+        self.data_name = data_name
+
+    def display(self):
+        print(self.data)
+
+    def cutter(self, col_name, point):
+        self.data[col_name] = self.data[col_name].round(point)
+
+    def cut(self):
+        for col in self.cols:
+            if 'MVA' in col:
+                self.cutter(col, 0)
+            else:
+                self.cutter(col, 3)
+
+    def save(self):
+        self.data.to_csv('data/'+self.data_name+"_trunc.csv")
+
+
+less = DataProcessor("data_full_trunc")
+less.display()
