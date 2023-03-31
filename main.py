09478bbcf24f7889c347b85ac1a33fd09e309e4e
Nabin Adhikari <34744818+nabinadhikariofficial@users.noreply.github.com>
2023-03-31 16:20:32 +0545

    main edit from web

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from generator import DataProcessor
+import pickle
 from sklearn.model_selection import train_test_split
 
 from sklearn.preprocessing import LabelEncoder
@@ -19,32 +20,40 @@ class modal():
         self.labels = self.data.data[label_names]
         self.features = self.data.data[feature_names]
         self.train, self.test, self.train_labels, self.test_labels = train_test_split(
-            self.labels, self.features, test_size=0.2, random_state=42)
+            self.labels, self.features, test_size=0.2, random_state=30)
         self.modal_ = modal_name.fit(self.train, self.train_labels)
 
     def modal_accuracy(self):
         pred = self.modal_.predict(self.test)
-        print("Acurracy is: "+str(accuracy_score(self.test_labels, pred)))
+        return ("_Acurracy_"+str(accuracy_score(self.test_labels, pred)))
 
     def modal_mse(self):
         pred = self.modal_.predict(self.test)
-        print("Mean Squared Error is: " +
-              str(mean_squared_error(self.test_labels, pred)))
+        return ("_MSE_" + str(mean_squared_error(self.test_labels, pred)))
 
 # we can keep on adding to this list for different algo test
 
+
+algolistnames = ['GradientBoostingRegressor', 'HistGradientBoostingRegressor',
+                 'RandomForestClassifier', 'HistGradientBoostingClassifier',
+                 'GradientBoostingClassifier']
 
 algo_list = [multioutput.MultiOutputRegressor(ensemble.GradientBoostingRegressor()),
              multioutput.MultiOutputRegressor(
                  ensemble.HistGradientBoostingRegressor()),
              multioutput.MultiOutputClassifier(
                  ensemble.RandomForestClassifier()),
+             multioutput.MultiOutputClassifier(
+                 ensemble.HistGradientBoostingClassifier()),
              multioutput.MultiOutputClassifier(ensemble.GradientBoostingClassifier())]
 
 for algo in algo_list:
-    modal1 = modal('data_less_trunc', ['Total_MVA', 'Po_GFM_MVA'], [
-        'Po_SG_MVA', 'Po_GFL_MVA'], algo)
+    modal1 = modal('datas_trunc', ['Total_MVA', 'Po_GFM_MVA'], [
+                   'Po_SG_MVA', 'Po_GFL_MVA'], algo)
     try:
-        modal1.modal_accuracy()
+        temp = modal1.modal_accuracy()
     except:
-        modal1.modal_mse()
+        temp = modal1.modal_mse()
+    print(temp)
+    filename = 'modal/'+algolistnames[algo_list.index(algo)]+temp+'.pkl'
+    pickle.dump(modal1.modal_, open(filename, 'wb'))
